eea4d3dec4587d700d9ba2e057a53deeebbe6a40
Shubham <500060901@stu.upes.ac.in>
2018-03-27 21:13:18 +0530

    first commit

---
+++ arraysum.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{    
+int i,size; 
+float array[size],sum = 0;
+printf("Enter the size of array");
+scanf("%d",&size);
+printf("Enter the integer elements of the array");
+for(i=0;i<size;i++)
+{
+ scanf("%f",&array[i]);
+}
+for(i=0;i<size;i++)
+{
+ sum+=array[i];
+}
+printf("the sum of the elements of the array is %.3f ", sum);
+return 0;
+}                                  
+
+	
