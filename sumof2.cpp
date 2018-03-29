2a2654311f1176a69fe350bdf9877751f2077eaf
Shubham <500060901@stu.upes.ac.in>
2018-03-30 01:15:47 +0530

    first commit

---
+++ sumof2.cpp
@@ -0,0 +1,14 @@
+//Program to enter two numbers and print their sum
+#include<iostream>
+using namespace std;
+int main()
+{
+	float first,second,sum;
+	cout<<"enter the first number";
+	cin>>first;
+	cout<<"enter the second number";
+	cin>>second;
+	sum=first+second;
+	cout<<"The sum of the entered number is"<<sum;
+	return 0;
+}
