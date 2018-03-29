50c8dac7c9f6d34dd455600297699d76bdaf0565
Shubham <500060901@stu.upes.ac.in>
2018-03-30 02:00:16 +0530

    Even odd number

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+//program to check whether the input number is even or odd 
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter a number"<<endl;
+	cin>>num;
+	if (num%2 == 0)
+	{cout<<"The entered number is Even"<<endl;
+	}
+	else
+	{cout<<"The entered number is Odd"<<endl;
+	}
+return 0;
+}
