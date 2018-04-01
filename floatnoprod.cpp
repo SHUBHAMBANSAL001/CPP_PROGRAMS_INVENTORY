2e83a366f80c72ac78a6739ab5ae95713a12ca3a
Shubham <500060901@stu.upes.ac.in>
2018-04-01 19:22:34 +0530

    product of two floating point numbers

---
+++ floatnoprod.cpp
@@ -0,0 +1,13 @@
+//program to multiply two floating point numbers and print the product
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"enter two numbers " <<endl;
+	float a,b;
+	cin>>a>>b;
+	float prod;
+	prod=a*b;
+	cout<<"The product of the entered two numbers is "<<prod;
+	return 0;	
+}
