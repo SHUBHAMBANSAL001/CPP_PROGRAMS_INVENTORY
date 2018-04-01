c0ba85e27638f3bf856b2743da21063bc5815345
Shubham <500060901@stu.upes.ac.in>
2018-04-01 19:20:52 +0530

    for ascii values of a character

---
+++ ascii.cpp
@@ -0,0 +1,14 @@
+//basic c program to print ascii value of the character entered
+#include<iostream>
+using namespace std;
+int main()
+{ 
+char a;
+cout<<"enter a character and its ascii value will be shown later"<<endl;
+cin>>a;
+int b;
+b=a;
+cout<<"its ascii value is "<<b<<endl;
+cout<<"(in case you enter two characters then only first character will be recorded)";
+return 0;
+}
