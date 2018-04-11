bd01222d9245135cae0f746e70a4fe1fb80d6d5e
Shubham <500060901@stu.upes.ac.in>
2018-04-11 20:32:06 +0530

    Check if number is pallindrome

---
+++ pallindrome.cpp
@@ -0,0 +1,21 @@
+//program to check if the given number is pallindrome or not
+#include<iostream>
+using namespace std;
+int main()
+{
+	int orignum,revnum,remainder,N;
+	cout<<"Enter the number for which you want to check the pallindrome";
+	cin>>orignum;
+	N=orignum;
+	while(orignum!=0)
+	{
+		remainder=orignum%10;
+		revnum=revnum*10+remainder;
+		orignum/=10;
+	}
+if(N==revnum)
+cout<<"The given number is a pallindrome";
+else
+cout<<"The given number is not a pallindrome";
+return 0;
+}
