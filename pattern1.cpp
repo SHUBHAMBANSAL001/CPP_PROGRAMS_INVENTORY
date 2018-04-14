4f18f5953434c0f19718ffa2431c771b91252c89
Shubham <500060901@stu.upes.ac.in>
2018-04-14 11:08:42 +0530

    Pattern number1

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,k=1;
+	int sp;
+	for(i=5;i>0;i--)
+ { 
+	for(sp=i-1;sp>0;sp--)
+	{
+		cout<<" ";
+	}
+    for(j=k;j>0;j--)
+	{
+		cout<<"#";
+	}
+	cout<<endl;    
+	k=k+2;
+ }
+	return 0;
+}
