4b9ab41c7e4f2737ceba8369076ca75515f40dcb
Shubham <500060901@stu.upes.ac.in>
2018-04-18 22:23:33 +0530

    Patter type 3

---
+++ pattern3.cpp
@@ -0,0 +1,19 @@
+//program to build pattern type 3 
+#include<iostream>
+using namespace std;
+int main()
+{ int i,j,count =0;
+for(i=1;i<5;i++)
+{	
+for(j=1;j<=i;j++)
+{count++;
+cout<<count;
+}
+cout<<endl;
+}
+return 0;
+}
+
+  
+
+
