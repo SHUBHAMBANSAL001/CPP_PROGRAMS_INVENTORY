de9d9c1ac96143851dbb09a1d0e860a4510148e7
Shubham <500060901@stu.upes.ac.in>
2018-04-23 11:06:29 +0530

    fibonacci upto certain number

---
+++ fibo_num.cpp
@@ -0,0 +1,22 @@
+//program to generate fibonacci series upto a certain number
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,a=0,b=1,sum=0,n;
+	cout<<"Enter the number upto which you want to display the series ";
+	cin>>n;
+	cout<<a<<endl<<b<<endl;
+	while(1)
+	{
+		sum =a+b;
+		if(sum>n)
+		break;
+		cout<<sum<<endl;
+		if(a<b)
+		a=sum;
+		else
+		b=sum;
+	}
+	return 0;
+}
