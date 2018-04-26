c5b705b478c6818e492093da9f19470bf2de0c05
Shubham <500060901@stu.upes.ac.in>
2018-04-26 10:10:30 +0530

    function sum natural numbers

---
+++ func_sum_natural.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void sum(int);
+int main()
+{
+	int n;
+	cout<<"Enter the number for which you want the sum from 1 "<<endl;
+	cin>>n;
+	sum(n);
+	return 0;
+}
+void sum(int a)
+{
+    int sum=0;
+	for(int i=1;i<=a;i++)
+	sum+=i;
+	cout<<"The sum of the natural numbers upto the given number is"<<endl;
+	cout<<sum;
+}
