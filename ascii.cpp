2134356e1c4b51509313ae5b9be4d18961d58b62
Shubham Bansal <500060901@stu.upes.ac.in>
2019-09-29 09:48:17 +0530

    Ascii value of a char

--- ascii.cpp
+++ ascii.cpp
@@ -6,9 +6,7 @@ int main()
 char ch;
 cout<<"enter a character and its ascii value will be shown later"<<endl;
 cin>>ch;
-int b;
-b=ch;
-cout<<"its ascii value is "<<b<<endl;
-cout<<"(in case you enter two characters then only first character will be recorded)";
+cout<<"its ascii value is "<<(int)ch<<endl;
+cout<<"Note: In case you enter two characters then only first character will be recorded";
 return 0;
 }
