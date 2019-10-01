1be1a2f2075fdcf62e979a383e06cf7987bfe4b5
Shubham Bansal <500060901@stu.upes.ac.in>
2019-10-01 19:01:43 +0530

    Dynamic memory Allocation

--- Dyn_mem_alloc.cpp
+++ Dyn_mem_alloc.cpp
@@ -1,4 +1,4 @@
-//program to dynamically allocate memory
+//Aim: Program to dynamically allocate memory
 #include<iostream>
 using namespace std;
 int main()
@@ -6,7 +6,7 @@ int main()
 	int num,*pointer,i;
 	cout<<"Enter how many elements you want to store in the array";
 	cin>>num;
-	pointer =  new int[num];
+	pointer =  new int[num];//The crucial statement
 	cout<<"Input "<<num<<" numbers now \n";
 	for(i=0;i<num;i++)
 	{
@@ -15,7 +15,7 @@ int main()
     cout<<"The elements entered and stored in the array are \n";
     for(i=0;i<num;i++)
     cout<<pointer[i]<<endl;
-   delete[] pointer;
+   delete[] pointer; //Free the memory when you no longer use it.
    return 0;
   
 }
