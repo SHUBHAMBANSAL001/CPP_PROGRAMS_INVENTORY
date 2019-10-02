f3b1293c979798fd23aad7e9e63ed1773bf40033
Shubham Bansal <500060901@stu.upes.ac.in>
2019-10-02 23:56:00 +0530

    Scale Conversion

--- fahrenhiet.cpp
+++ fahrenhiet.cpp
@@ -1,10 +1,10 @@
-//program to print the farenhiet in celcius temperature
+//Aim: Program to print the farenhiet in celcius temperature
 #include<iostream>
 using namespace std;
 int main()
 {
-	float f,c;
-	cout<<"Enter the temperature in fahrenheit";
+    float f,c;
+    cout<<"Enter the temperature in fahrenheit";
     cin>>f;
     c= 0.555*(f-32.00);
     cout<<"The value of entered temperature in celsius is"<<c;
