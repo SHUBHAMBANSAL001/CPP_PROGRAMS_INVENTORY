029bf92baa39d1291bfc9ef976c03d4be3006cd6
Shubham Bansal <500060901@stu.upes.ac.in>
2019-10-09 09:46:04 +0530

    First Program

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+//Aim: The first program 
+#include<iostream> // Must Know the meaning of every symbol in this statement 
 using namespace std;
 int main()
 {
-cout<<"hello world";
+cout<<"hello world"; 
 return 0;
 }
 
